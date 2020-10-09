c0eb00fe10c94580c7e288ec10898d908ed482f1
dejsan <dexan.maric@gmail.com>
2020-10-09 23:28:39 +0200

    Implemented redux-persist

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux'
-import store from './store'
+import returnStoreAndPersistor from './store'
 
 import './assets/css/index.css';
 import App from './views/App';
+
+const { store } = returnStoreAndPersistor()
 
 const target = document.querySelector('#root')
 
--- src/store.js
+++ src/store.js
@@ -1,26 +1,32 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './reducers'
+import rootReducer from "./reducers";
 
-const initialState = {}
-const enhancers = []
-const middleware = []
+const initialState = {};
+const enhancers = [];
+const middleware = [];
 
-if (process.env.NODE_ENV === 'development') {
-    const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-    if (typeof devToolsExtension === 'function') {
-        enhancers.push(devToolsExtension())
-    }
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+if (process.env.NODE_ENV === "development") {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === "function") {
+    enhancers.push(devToolsExtension());
+  }
 }
 
-const composedEnhancers = compose(
-    applyMiddleware(...middleware),
-    ...enhancers
-)
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
 
-export default createStore(
-    rootReducer,
-    initialState,
-    composedEnhancers
-)
+export default () => {
+  let store = createStore(persistedReducer, initialState, composedEnhancers);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
