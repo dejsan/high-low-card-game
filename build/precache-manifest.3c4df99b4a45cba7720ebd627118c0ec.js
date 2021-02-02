1ec3907c399c80baa91baa7a0cc509ceab59296b
Dejan M <dexan.maric@gmail.com>
2021-02-02 16:30:36 +0100

    Added production build

---
+++ build/precache-manifest.3c4df99b4a45cba7720ebd627118c0ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8874bbf54bcd6e94dfa3b32015ad53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "387e9c9507003a7677ad",
+    "url": "/static/css/main.c45adcd2.chunk.css"
+  },
+  {
+    "revision": "1db20054ee06f0b2a224",
+    "url": "/static/js/2.03085360.chunk.js"
+  },
+  {
+    "revision": "69f8dcef8d7d4d3ab122841edcf3450b",
+    "url": "/static/js/2.03085360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387e9c9507003a7677ad",
+    "url": "/static/js/main.a8fb5bf4.chunk.js"
+  },
+  {
+    "revision": "be9ec0300b8344557765",
+    "url": "/static/js/runtime-main.4e7cba3f.js"
+  },
+  {
+    "revision": "311e03763c69262c0e000e99b31d5d4c",
+    "url": "/static/media/click.311e0376.mp3"
+  },
+  {
+    "revision": "7ae8136dd2343385d0501dab0c13f9a8",
+    "url": "/static/media/music.7ae8136d.mp3"
+  },
+  {
+    "revision": "49dc56c336dbbae311e343be31380b8d",
+    "url": "/static/media/table_background.49dc56c3.jpg"
+  }
+]);
